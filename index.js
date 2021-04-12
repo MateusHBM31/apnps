c51a9eed42573adea62dc6fa0e051003619becf0
MateusHBM31 <seuemail@seuemail.com>
2021-04-11 22:59:57 -0300

    Criação da rota layout

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get('/msg', function(req,res){
     res.send("Essa mensagem é automática!")
 })
 
+app.get('/layout', function(req,res){
+    res.send("Esse é meu novo Layout!")
+})
+
 app.listen(3000,function(){
     console.log('Conexão Inicializada')
 })
