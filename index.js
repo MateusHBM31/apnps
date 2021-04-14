221797f079669301923cf47dde427ad07153dc01
MateusHBM31 <seuemail@seuemail.com>
2021-04-14 19:50:52 -0300

    Configurações iniciais do projeto

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require('express')
 const app = express()
+var bodyParser = require('body-parser')
+var cookieParser = require('cookie-parser')
+var path = require('path')
 
-app.get('/', function(req,res){
+app.use(cookieParser())
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.set("view engine","ejs")
+
+app.use(express.static(path.join(__dirname, "public")))
+
+app.get('/', function(req,res){ 
     res.send("Olá Mateus!!")
 })
-
-    
+ 
 app.listen(3000,function(){
     console.log('Conexão Inicializada')
 })
